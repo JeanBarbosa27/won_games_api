dfccf131dc3b20b527534f8af78902d0de15351f
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-09-25 10:03:39 -0300

    Starting, configuring and structuring project
    Here the project was started with create strapi-app, configured with
    GraphQL and CKEditor plugins and had tables created with its columns.

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b65fee28bab7e849e82d711ca824a6e5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a5e9e856-dbfe-465c-b5ed-474f00725c74'
+};

35d3d711871a77ac0c9d375944a67340d01c200c
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-09-28 07:26:19 -0300

    Add logic to onw plugin

--- plugins/repositories/admin/src/containers/HomePage/index.js
+++ plugins/repositories/admin/src/containers/HomePage/index.js
@@ -1,19 +1,48 @@
-/*
- *
- * HomePage
- *
- */
+import React, { useState, useEffect, memo } from 'react';
+import { Header } from '@buffetjs/custom';
+import { Table } from '@buffetjs/core';
+import styled from 'styled-components';
+import axios from 'axios';
 
-import React, { memo } from 'react';
-// import PropTypes from 'prop-types';
-import pluginId from '../../pluginId';
+const Wrapper = styled.div`
+  padding: 18px 30px;
+
+  p {
+    margin-top: 1rem;
+  }
+`;
 
 const HomePage = () => {
+  const [rows, setRows] = useState([]);
+  const headers = [
+    {
+      name: 'Name',
+      value: 'name'
+    },
+    {
+      name: 'Description',
+      value: 'description'
+    },
+    {
+      name: 'URL',
+      value: 'html_url'
+    },
+  ];
+
+  useEffect(() => {
+    axios.get('https://api.github.com/users/React-Avancado/repos')
+      .then(response => setRows(response.data))
+      .catch(e => strapi.notification.error(`teste: ${e}`))
+  }, [])
+
   return (
-    <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
-    </div>
+    <Wrapper>
+      <Header
+        title={{ label: 'Repositories' }}
+        content="A list of our repositories in React Avançado Udemy course"
+      />
+      <Table headers={headers} rows={rows} />
+    </Wrapper>
   );
 };
 

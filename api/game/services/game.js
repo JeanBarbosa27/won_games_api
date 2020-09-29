93ca145c137e30932ac52141b13ffedf3fa2e645
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-09-29 07:34:20 -0300

    Starting get game infos and scraping game page

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -5,8 +5,37 @@
  * to customize this service
  */
 
+const axios = require('axios')
+
+async function getGameInfo(slug) {
+  const jsdom = require('jsdom')
+  const { JSDOM } = jsdom
+  const { data } = await axios.get(`https://www.gog.com/game/${slug}`)
+  const dom = new JSDOM(data)
+
+  const ratingElement = dom.window.document.querySelector('.age-restrictions__icon use')
+  const descriptionElement = dom.window.document.querySelector('.description')
+
+  return {
+    rating: ratingElement
+              ? ratingElement
+                  .getAttribute('xlink:href')
+                  .replace(/_/g, '')
+                  .replace(/[^\w-]+/g, '')
+              : 'br0',
+    short_description: descriptionElement.textContent.trim().slice(0, 160),
+    description: descriptionElement.innerHTML
+  }
+}
+
 module.exports = {
-  populate: async(params) => {
+  populate: async (params) => {
     console.log('Executing game service...')
+
+    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`
+
+    const { data : { products } } = await axios.get(gogApiUrl)
+
+    console.log(await getGameInfo(products[1].slug))
   }
 };
